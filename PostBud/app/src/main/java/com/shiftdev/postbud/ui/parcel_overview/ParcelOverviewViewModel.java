00dd3788b6d97753c6b3d8cb0b16034f0f9e7cc4
Daniel W <danieldwiebe@gmail.com>
2020-12-29 12:19:46 -0600

    file names adjusted for use compliance. implemented parcel overviews and naming conventions as well as a test query for firestore.

--- PostBud/app/src/main/java/com/shiftdev/postbud/ui/parcel_overview/ParcelOverviewViewModel.java
+++ PostBud/app/src/main/java/com/shiftdev/postbud/ui/parcel_overview/ParcelOverviewViewModel.java
@@ -1,14 +1,14 @@
-package com.shiftdev.postbud.ui.dashboard;
+package com.shiftdev.postbud.ui.parcel_overview;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ParcelOverviewViewModel extends ViewModel {
 
      private MutableLiveData<String> mText;
 
-     public DashboardViewModel() {
+     public ParcelOverviewViewModel() {
           mText = new MutableLiveData<>();
           mText.setValue("This is dashboard fragment");
      }
