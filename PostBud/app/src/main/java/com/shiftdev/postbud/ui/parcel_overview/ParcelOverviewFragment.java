395ca3c3d3d8a06b00a502497bfd3602f7f1b9e0
Daniel W <danieldwiebe@gmail.com>
2020-12-30 11:37:30 -0600

    * adjusted bottom nav view elements to not crash on open. Stable build now

--- PostBud/app/src/main/java/com/shiftdev/postbud/ui/parcel_overview/ParcelOverviewFragment.java
+++ PostBud/app/src/main/java/com/shiftdev/postbud/ui/parcel_overview/ParcelOverviewFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -26,6 +29,13 @@ public class ParcelOverviewFragment extends Fragment {
           parcelOverviewViewModel =
                   new ViewModelProvider(this).get(ParcelOverviewViewModel.class);
           View root = inflater.inflate(R.layout.fragment_parcel_overview, container, false);
+          final TextView textView = root.findViewById(R.id.text_dashboard);
+          parcelOverviewViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+               @Override
+               public void onChanged(@Nullable String s) {
+                    textView.setText(s);
+               }
+          });
           return root;
      }
 }
